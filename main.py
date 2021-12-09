22abda01869c6adf5b75b2a0783f22de818e6878
Cameron Koegel <ckoegel@bandwidth.com>
2021-12-09 16:03:18 -0500

    DX-2325 Overhaul Sample for New SDK

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
 import os
 
+# needs to be changed when sdk becomes python package
+import sys
+sys.path.insert(0, 'C:/Users/ckoegel/Documents/sdk-generation/OpenAPI Specs/next-gen-sdk-tests/python')
+import bandwidth_python
+from bandwidth_python.api.messages_api import MessagesApi
+from bandwidth_python.model.message_request import MessageRequest
+# ---------------------------------------------------
+
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from bandwidth.bandwidth_client import BandwidthClient
-from bandwidth.messaging.models.message_request import MessageRequest
 
+
+BW_NUMBER = os.environ.get('BW_NUMBER')
 BW_USERNAME = os.environ.get('BW_USERNAME')
 BW_PASSWORD = os.environ.get('BW_PASSWORD')
-BW_NUMBER = os.environ.get('BW_NUMBER')
 BW_ACCOUNT_ID = os.environ.get('BW_ACCOUNT_ID')
 BW_MESSAGING_APPLICATION_ID = os.environ.get('BW_MESSAGING_APPLICATION_ID')
 
 
-bandwidth_client = BandwidthClient(
-    messaging_basic_auth_user_name=BW_USERNAME,
-    messaging_basic_auth_password=BW_PASSWORD
-)
-messaging_client = bandwidth_client.messaging_client.client
-
-account_id = BW_ACCOUNT_ID
-
-
-class CreateBody(BaseModel):
+class CreateBody(BaseModel):    # model for the received json body to create a message
     to: str
     text: str
 
 
-class InboundBody(BaseModel):
-    type: str
-    description: str
-    message: dict
+configuration = bandwidth_python.Configuration(     # needs to be updated*********  # Configure HTTP basic authorization: httpBasic
+    username=BW_USERNAME,
+    password=BW_PASSWORD
+)
+
+
+api_client = bandwidth_python.ApiClient(configuration)  # needs to be updated*********
+messages_api_instance = MessagesApi(api_client) # needs to be updated*********
 
 
 app = FastAPI()
 
 
-@app.post('/callbacks/outbound/messaging')
-def handle_outbound_message(create_body: CreateBody):
-    body = MessageRequest()
-    body.application_id = BW_MESSAGING_APPLICATION_ID
-    body.to = [create_body.to]
-    body.mfrom = BW_NUMBER
-    body.text = create_body.text
+@app.post('/sendMessage')
+def send_message(create_body: CreateBody):
+    message_body = MessageRequest(  # needs to be updated*********
+        to=[create_body.to],
+        _from=BW_NUMBER,
+        application_id=BW_MESSAGING_APPLICATION_ID,
+        text=create_body.text
+    )
+    response = messages_api_instance.create_message(    # needs to be updated*********
+        account_id=BW_ACCOUNT_ID,
+        message_request=message_body,
+        _return_http_data_only=False
+    )
 
-    create_response = messaging_client.create_message(account_id, body=body)
-
-    return create_response.status_code
+    return response[1]
 
 
 @app.post('/callbacks/outbound/messaging/status')
@@ -53,9 +59,9 @@ async def handle_outbound_status(request: Request):
     status_body_array = await request.json()
     status_body = status_body_array[0]
     if status_body['type'] == "message-sending":
-        print("message-sending type is only for MMS")
+        print("message-sending type is only for MMS.")
     elif status_body['type'] == "message-delivered":
-        print("your message has been handed off to the Bandwidth's MMSC network, but has not been confirmed at the downstream carrier")
+        print("your message has been handed off to the Bandwidth's MMSC network, but has not been confirmed at the downstream carrier.")
     elif status_body['type'] == "message-failed":
         print("For MMS and Group Messages, you will only receive this callback if you have enabled delivery receipts on MMS.")
     else:
@@ -70,10 +76,10 @@ async def handle_inbound(request: Request):
     inbound_body = inbound_body_array[0]
     print(inbound_body['description'])
     if inbound_body['type'] == "message-received":
-        print("From: {}\nTo: {}\nText: {}".format(inbound_body['message']['from'], inbound_body['message']['to'][0],
+        print("To: {}\nFrom: {}\nText: {}".format(inbound_body['message']['to'][0], inbound_body['message']['from'],
                                                   inbound_body['message']['text']))
     else:
-        print("Message type does not match endpoint. This endpoint is used for inbound messages only.\nOutbound message callbacks should be sent to /callbacks/outbound/messaging.")
+        print("Message type does not match endpoint. This endpoint is used for inbound messages only.\nOutbound message status callbacks should be sent to /callbacks/outbound/messaging/status.")
 
     return 200
 
